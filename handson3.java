1f729263cc3515a9ef8b703ef4e9788b4914c7f4
clerizedd <casucolerizedoreen+github@gmail.com>
2022-12-17 04:51:41 +0800

    Update handson3.java

--- handson3.java
+++ handson3.java
@@ -4,20 +4,14 @@ public class handson3 {
         Scanner sc = new Scanner(System.in);
         int y=0;
         String x;
-        String a = "java";
         String b = "java string";
 
     while (y < 3){
         System.out.print ("Enter a String: ");
         x = sc.nextLine() ;
-        if (x == a) {
-            System.out.println ("Java is the best programming language");        }
-        else if (x == b){
-            System.out.println ("This is an example of Java Switch using String");      }
-        else {
-            System.out.println ("Sorry, please enter the different word");        }
- 
+        
+        if (x=="java"){
+            System.out.print ("Java is the best programming language");
         }
-        y++;
-    }
-}
+
+    }}}
